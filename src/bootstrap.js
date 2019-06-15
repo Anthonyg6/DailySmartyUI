057335b6ff6225e341f557ff13768af8b0ed6f49
Anthonyg6 <anthonygallegos6@gmail.com>
2019-06-14 20:58:14 -0600

    changed app.js to home.js and added routes for app

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,22 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import App from './components/app';
-import reducers from './reducers';
+import Home from "./components/home";
+import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-import 'bootstrap/dist/css/bootstrap.css';
-import './style/main.scss';
+import "bootstrap/dist/css/bootstrap.css";
+import "./style/main.scss";
 
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-      <App />
-    </Provider>
-    , document.querySelector('.app-wrapper'));
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/results" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>,
+    document.querySelector(".app-wrapper")
+  );
 }
 
-document.addEventListener('DOMContentLoaded', main);
+document.addEventListener("DOMContentLoaded", main);
