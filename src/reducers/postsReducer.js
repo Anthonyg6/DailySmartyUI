7485f920cf6545639e3b66b30ddc2b9dcb06e453
Anthonyg6 <anthonygallegos6@gmail.com>
2019-06-17 20:35:16 -0600

    set up redux dev tools and fixed reducer for set_recent_posts

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,15 +1,19 @@
-import { set_recent_posts} from "../actions/types";
+import { set_recent_posts } from "../actions/types";
 
 const init_state = {
-    posts: [],
-    recentPosts: []
+  posts: [],
+  recentPosts: []
 };
 
-export default function(state= init_state, action) {
-    switch(action,type) {
-        case set_recent_posts:
-            return [...state, recentPosts: action.payload]
-        default:
-            return state;
-    }
+export default function(state = init_state, action) {
+  switch (action.type) {
+    case set_recent_posts:
+      const recentPosts = action.payload;
+      return {
+        ...state,
+        recentPosts
+      };
+    default:
+      return state;
+  }
 }
