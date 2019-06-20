3154802b3143017f550faa96b8d0a7b9c9410f78
Anthonyg6 <anthonygallegos6@gmail.com>
2019-06-19 21:16:53 -0600

    modified post component to render a result post and set up links

--- src/components/posts.js
+++ src/components/posts.js
@@ -12,10 +12,9 @@ class RecentPosts extends Component {
   renderPosts = function() {
     const posts = this.props.recentPosts.map((post, index) => {
       if (index < 3) {
-        return <Post {...post} key={index} />;
+        return <Post type="recent" {...post} key={index} />;
       }
     });
-
     return posts;
   };
   render() {
